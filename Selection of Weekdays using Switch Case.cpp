7f1242c7d351eaedefd594b027bb20ab26b57ef7
Siddhant Bhosale <siddhant.bhosale05@gmail.com>
2024-10-12 16:02:08 +0530

    Selection of Weekdays using Switch Case.cpp

--- Selection of Weekdays using Switch Case.cpp
+++ Selection of Weekdays using Switch Case.cpp
@@ -1,6 +1,6 @@
 //Name:Siddhant Kiran Bhosale
 //PRN:23070123038
-//Aim:Implemention of Decision Making Statements to program a Weekday Selection using Switch Case
+//Aim:Implementation of Decision Making Statements to program a Weekday Selection using Switch Case
 
 #include <iostream>
 using namespace std;
